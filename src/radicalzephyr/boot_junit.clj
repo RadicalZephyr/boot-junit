48bc31c359f4d68d527b1dd762c2006b8ce73c22
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-28 21:37:51 -0500

    Stub out a function for displaying the jUnit results better

--- src/radicalzephyr/boot_junit.clj
+++ src/radicalzephyr/boot_junit.clj
@@ -3,6 +3,9 @@
   (:require '[[boot.core :as core]])
   (:import org.junit.runner.JUnitCore))
 
+(defn destructure-results [result]
+  {})
+
 (deftask junit
   "Run the jUnit test runner."
   [p packages PACKAGE #{sym} "The set of Java packages to run tests in."]
@@ -10,5 +13,6 @@
     (let [result (JUnitCore/runClasses
                   (into-array [#_ (magic goes here to find all test classes)]))]
       (when (> (.getFailureCount result) 0)
-        (throw (ex-info "There were some test failures." {:result result}))))
+        (throw (ex-info "There were some test failures."
+                        (destructure-results result)))))
     fileset))
