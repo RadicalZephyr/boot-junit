1899025e5f0872a63a211b266709531a9afb03f1
Geoff Shannon <geoffpshannon@gmail.com>
2015-07-11 19:33:18 -0500

    Add a switch for running a set of test classes by name

--- src/radicalzephyr/boot_junit.clj
+++ src/radicalzephyr/boot_junit.clj
@@ -5,11 +5,12 @@
             [clojure.string :as str]))
 
 (def pod-deps
-  '[[radicalzephyr/cljunit "0.1.0"]])
+  '[[radicalzephyr/cljunit "0.1.1-SNAPSHOT"]])
 
 (defn- init [fresh-pod]
   (pod/with-eval-in fresh-pod
-    (require '[cljunit.core :refer [run-tests-in-packages]])))
+    (require '[cljunit.core :refer [run-test-classes
+                                    run-tests-in-packages]])))
 
 (defn- path->package-name [path]
   (str/join "." (-> path
@@ -24,7 +25,8 @@
 
 (core/deftask junit
   "Run the jUnit test runner."
-  [p paths    PATH  #{str} "The set of source paths to search for tests."]
+  [p paths    PATH  #{str} "The set of source paths to search for tests."
+   c classes  CLASS #{str} "The set of Java class names to run as tests."]
   (let [worker-pods (pod/pod-pool (update-in (core/get-env) [:dependencies] into pod-deps)
                                   :init init)]
     (core/cleanup (worker-pods :shutdown))
@@ -32,10 +34,18 @@
       (let [worker-pod (worker-pods :refresh)
             packages (or (seq (map path->package-name paths))
                          (all-packages (core/input-files fileset)))]
-        (if (seq packages)
+        (cond
+          (seq classes)
+          (let [result (pod/with-eval-in worker-pod
+                         (run-test-classes '~packages '~classes))]
+            (when (> (:failures result) 0)
+              (throw (ex-info "Some tests failed or errored" {}))))
+
+          (seq packages)
           (let [result (pod/with-eval-in worker-pod
                          (run-tests-in-packages '~packages))]
             (when (> (:failures result) 0)
               (throw (ex-info "Some tests failed or errored" {}))))
-          (println "No packages were tested.")))
+
+          :else (println "No packages were tested.")))
       fileset)))
