b0a489b226c322f27e9fb8505bdffe3307fc42f5
Geoff Shannon <geoffpshannon@gmail.com>
2015-07-14 11:45:56 -0500

    Add a junit test suite class for testing

---
+++ src/radicalzephyr/boot_junit/test/SuiteTest.java
@@ -0,0 +1,9 @@
+package radicalzephyr.boot_junit.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculatorTest.class})
+public class SuiteTest {
+}
